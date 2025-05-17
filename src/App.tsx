766bb3cf49f1d27606319ce248d23c265bec917a
Tademola7862 <tijaniahamzat@gmail.com>
2025-05-17 15:45:48 -0700

    first flow commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home'
+// import { Button } from 'flowbite-react'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>FLOW</h1>
+      {/* Home */}
+      <Home />
+     
     </>
   )
 }
